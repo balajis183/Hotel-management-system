de12f3cbb81fbff67f4ed39f4e2fc47b9aea6fcb
balajis183 <sbalaji2000s@gmail.com>
2025-07-11 11:21:16 +0530

    updated client side url

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://13.202.204.246', // Your Express server URL
-  // baseURL: 'http://localhost:80', // Your Express server URL
+  // baseURL: 'http://13.202.204.246', // Your Express server URL
+  baseURL: 'https://stayhub-hotel-management-system.onrender.com', // Your Express server URL
 
 });
 
